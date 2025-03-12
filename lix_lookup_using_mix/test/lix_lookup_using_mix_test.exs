1a2c2a1d49f24ff5bb27da770be140399cfcd9de
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-03-12 10:30:43 +0100

    Test that script runs to end producing output files

--- lix_lookup_using_mix/test/lix_lookup_using_mix_test.exs
+++ lix_lookup_using_mix/test/lix_lookup_using_mix_test.exs
@@ -2,7 +2,9 @@ defmodule LixLookupUsingMixTest do
   use ExUnit.Case
   doctest LixLookupUsingMix
 
-  test "greets the world" do
-    assert LixLookupUsingMix.hello() == :world
+  test "all runs smoothly" do
+    str = LixLookupUsingMix.main()
+    assert is_struct(str)
+    assert str.path == "./region_staff_no_email_found.csv"
   end
 end
