9aac4ee6bd40fd84524a04a12fda58a06ef321a3
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-03-17 20:30:43 +0100

    Create processes to parse csv file with regional staff data, and perform lookup

--- lix_lookup_using_mix/lib/lookup.ex
+++ lix_lookup_using_mix/lib/lookup.ex
@@ -1,7 +1,14 @@
 
 defmodule LixLookup do
   @pwd "./"
-  @all_staff_list  @pwd<>"all_staff.csv"
+  @all_staff_list  @pwd<>"all_staff2.csv"
+  @region_staff_list  @pwd<>"region_staff_list.csv"
+
+  def run do
+    {time, result} = :timer.tc(fn -> main() end)
+    IO.puts("Execution time: #{time / 1_000_000} seconds")
+    result
+  end
 
   def main() do
     {:ok, success_process_counter} = ProcessCounter.start_link(0)
@@ -17,9 +24,17 @@ defmodule LixLookup do
       |> Enum.reduce(%{}, &Map.merge(&2, &1)) # Merges the results of all tasks
       |> Staff.start_link()
 
-      Staff.find_staff_email(staff_cache, [844, 10089,"Celestine Augustina Chinedu","Customer Relationship Assistant"])
-      |> IO.inspect(label: "MATCH...")
-      # proc_summary(success_process_counter, error_process_counter)
+    @region_staff_list
+    |> line_stream_from_chunk_read()
+    |> Stream.chunk_every(100)
+    |> Task.async_stream(
+      &match_staff_to_email(staff_cache, &1),
+      max_concurrency: 5,
+      timeout: :infinity
+    )
+    |> Enum.reduce([], fn ({:ok, record}, acc) -> acc ++ record end)
+    |> Enum.reduce([], fn ({key, value}, acc) -> merge({key, value}, acc) end)
+    |> write_stream_to_csv(@region_staff_emails, use_headers: true)
   end
 
   @doc """
@@ -43,9 +58,7 @@ defmodule LixLookup do
       try do
         map =
           lines
-          |> Stream.map(&String.trim/1)
-          |> Stream.map(&String.split(&1, ","))
-          # |> Stream.reject(fn row -> Enum.at(row, 7) == nil end)
+          |> format_string()
           |> Stream.map(fn([_, _, _, _, _, id, _, email | _]) ->
             %{id => String.downcase(email)}
           end)
@@ -65,6 +78,39 @@ defmodule LixLookup do
         ProcessCounter.increment_count(error_counter_pid)
         %{}
     end
+  end
+
+  def format_string(enum) do
+    enum
+    |> Stream.map(&String.trim(&1))
+    |> Stream.map(&String.split(&1, ",", trim: true))
+  end
+
+  def match_staff_to_email(cache_pid, staff_list) do
+    staff_list
+    |> format_string()
+    |> Enum.reject(fn (row) -> row == [] end )
+    |> Enum.map(&Staff.find_staff_email(&1, cache_pid))
+  end
+
+  def merge({key, {id, name, email}}, acc) when key != :error do
+    acc ++ ["#{id}, #{String.trim(name)}, #{email}\n"]
+  end
+
+  def merge(_, acc) do
+    acc
+  end
+
+  defp write_stream_to_csv(stream_data, csv_path, opts \\ [use_headers: false]) do
+    headers = ["staff_id, name, email\n"]
+    use_headers = Keyword.get(opts, :use_headers, false)
+
+    if use_headers == true do
+      Stream.concat(headers, stream_data)
+    else
+      stream_data
+    end
+    |> Enum.into(File.stream!(csv_path))
   end
 
   def proc_summary(success_process_counter, error_process_counter) do
