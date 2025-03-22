030a8af0df8489e5cad8ffb613298f0889f37c13
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-03-22 08:18:53 +0100

    Minor edits

--- lix_lookup_using_mix/lib/lookup.ex
+++ lix_lookup_using_mix/lib/lookup.ex
@@ -30,7 +30,7 @@ defmodule LixLookup do
     all_staff
     |> line_stream_from_chunk_read()
     |> Stream.chunk_every(5000)
-    |> Task.async_stream(&build_and_cache_map(&1, pid), max_concurrency: 8, timeout: :infinity)
+    |> Task.async_stream(&build_and_cache_map(&1, pid), max_concurrency: 8, timeout: 30_000)
     |> Stream.run()
   end
 
@@ -39,7 +39,7 @@ defmodule LixLookup do
     and writes the matched data to a CSV file.
 
     ## Parameters
-    - `region_staff`: The input data (stream or chunked) representing region staff.
+    - `region_staff`: The input data CSV file path containing region staff.
     - `path`: The file path where the CSV output will be written.
     - `staff_cache_pid`: The PID of the cache process used to look up staff emails.
   """
@@ -49,7 +49,7 @@ defmodule LixLookup do
     |> Stream.chunk_every(5000)
     |> Task.async_stream(&match_staff_to_email(&1, staff_cache_pid),
       max_concurrency: 8,
-      timeout: :infinity
+      timeout: 30_000
     )
     |> Stream.run()
 
