6f12c59ea4b7d55854404da4a96aa4617bd5c5d9
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-03-24 12:28:40 +0100

    Refactor StaffCache api and internal implementation

--- lix_lookup_using_mix/lib/cache.ex
+++ lix_lookup_using_mix/lib/cache.ex
@@ -59,34 +59,34 @@ defmodule StaffCache do
       matched_staff end)
   end
 
-  def update_all_staff(agent, staff) when is_map(staff) do
+  def add_staff(agent, staff) when is_map(staff) do
     Agent.update(agent, fn {all_staff, matched_staff} ->
       {Map.merge(staff, all_staff), matched_staff}
     end)
   end
 
-  def update_matched_staff(staff, agent) do
-    match_staff_id_to_emails = fn staff, all_staff ->
-      staff
-      |> Enum.map(fn [_, id, name, _] ->
-          email = Map.get(all_staff, id)
-          if email == nil do
-            {:error, "Staff ID does not match any record."}
-          else
-            {:ok, "#{id}, #{String.trim(name)}, #{email}\n"}
-          end
-        end)
-      |> Enum.map(fn {tag, row} ->
-        case tag do
-          :ok -> row
-          :error -> []
+  def match_staff(agent, staff) do
+    Agent.update(agent, fn {all_staff, matched_staff} ->
+      new_matched_staff = match_staff_id_to_emails(staff, all_staff)
+      {all_staff, matched_staff ++ new_matched_staff}
+    end)
+  end
+
+  defp match_staff_id_to_emails(staff, all_staff) do
+    staff
+    |> Enum.map(fn [_, id, name, _] ->
+        email = Map.get(all_staff, id)
+        if email == nil do
+          {:error, "Staff ID does not match any record."}
+        else
+          {:ok, "#{id}, #{String.trim(name)}, #{email}\n"}
         end
       end)
-    end
-
-    Agent.update(agent, fn {all_staff, matched_staff} ->
-      new_matched_staff = match_staff_id_to_emails.(staff, all_staff)
-      {all_staff, matched_staff ++ new_matched_staff}
+    |> Enum.map(fn {tag, row} ->
+      case tag do
+        :ok -> row
+        :error -> []
+      end
     end)
   end
 end
