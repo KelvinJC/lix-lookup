46a187d91345a9e410d43f13697c14cc7c92bf24
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-02 12:25:28 +0100

    Fix Genserver callbacks

--- lix_lookup_using_mix/lib/cache.ex
+++ lix_lookup_using_mix/lib/cache.ex
@@ -13,12 +13,7 @@ defmodule StaffCacheRegister do
   end
 
   @impl true
-  def handle_call({:list}, _from, caches) do
-    {:reply, caches}
-  end
-
-  @impl true
-  def handle_cast({:create, num_caches}, caches) do
+  def handle_cast({:create, num_caches}, _caches) do
     pids_of_caches =
       for _ <- 1..num_caches do
         {:ok, pid} = StaffCache.start_link()
@@ -29,19 +24,31 @@ defmodule StaffCacheRegister do
 
   @impl true
   def handle_cast({:add, num_caches}, caches) do
-    pids_of_new_caches =
-      for _ <- 1..num_caches do
-        {:ok, pid} = StaffCache.start_link()
-        pid
-      end
-    {:noreply, pids_of_new_caches ++ caches}
+    case caches do
+      [] ->
+        {:noreply, caches}
+      _ ->
+        pids_of_new_caches =
+          for _ <- 1..num_caches do
+            {:ok, pid} = StaffCache.start_link()
+            pid
+          end
+        {:noreply, pids_of_new_caches ++ caches}
+    end
+  end
+
+  @impl true
+  def handle_call({:list}, _from, caches) do
+    {:reply, caches, caches}
   end
 
   @impl true
   def handle_call({:get_cache_by_index, index}, _from, caches) do
     default_cache = Enum.at(caches, 0)
-    Enum.at(caches, index, default_cache)
+    cache_pid = Enum.at(caches, index, default_cache)
+    {:reply, cache_pid, caches}
   end
+
 end
 
 defmodule StaffCache do
