0b684869f0c7fec58cbb77f2741a2ba80d0b5017
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-03 10:38:18 +0100

    Test GenServer returns only live agents

--- lix_lookup_using_mix/test/register_test.exs
+++ lix_lookup_using_mix/test/register_test.exs
@@ -10,8 +10,20 @@ defmodule StaffCacheRegisterTest do
     assert StaffCacheRegister.list(register) == []
 
     StaffCacheRegister.create(register, 5)
-    pids = StaffCacheRegister.list(register)
-    assert pids != []
-    assert length(pids) == 5
+    caches = StaffCacheRegister.list(register)
+    assert caches != []
+    assert length(caches) == 5
+  end
+
+  test "returns only live agents", %{register: register} do
+    StaffCacheRegister.create(register, 5)
+    cache = StaffCacheRegister.get_cache_by_index(register, 4)
+    assert is_pid(cache) == true
+
+    Agent.stop(cache)
+
+    fallback_cache = StaffCacheRegister.get_cache_by_index(register, 4)
+    assert fallback_cache != cache
+    assert is_pid(fallback_cache) == true
   end
 end
